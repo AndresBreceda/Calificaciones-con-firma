3f9d6597ef612374ea576e0fbbc21624b7e56d12
Efras2785 <efrasma@gmail.com>
2026-07-01 19:11:21 -0600

    Ya nomas falta agregar más diseño

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
+    # Rutas de Autenticación
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    # Rutas del Sistema
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('calificaciones/', views.grade_entry, name='grade_entry'),
-    path('usuarios/', views.user_management, name='user_management'),
-    path('firmar/', views.digital_signature, name='digital_signature'),
+    path('curso/<int:curso_id>/calificaciones/', views.grade_entry, name='grade_entry'),
+    path('curso/<int:curso_id>/iniciar-firma/', views.iniciar_firma, name='iniciar_firma'),
+    path('curso/<int:curso_id>/firmar/', views.digital_signature, name='digital_signature'),
+    path('curso/<int:curso_id>/pdf/', views.generar_pdf, name='generar_pdf'),
 ]
